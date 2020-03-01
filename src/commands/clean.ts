cdbc4ad16bdea9d9595668d7737182caf346c7a7
SplitWolf <mat.jake1360@gmail.com>
2020-03-01 09:53:49 -0500

    refactor

--- src/commands/clean.ts
+++ src/commands/clean.ts
@@ -1,31 +1,42 @@
-import { Command } from "./base"
+import { Command } from "./base";
 import { Message } from "discord.js";
 
 module.exports = class clean extends Command {
-    constructor() {
-        super({name: "clean", description: "", usage: "", group: "member", aliases: [], userPermissions: ['MANAGE_MESSAGES']})
+  constructor() {
+    super({
+      name: "clean",
+      description: "",
+      usage: "",
+      group: "member",
+      aliases: [],
+      userPermissions: ["MANAGE_MESSAGES"]
+    });
+  }
+
+  async run(
+    bot: import("discord.js").Client,
+    message: import("discord.js").Message,
+    args: string[],
+    prefix: string
+  ): Promise<void> {
+    message.delete();
+    let amount: number = Number(args[0]);
+    if (isNaN(amount) || amount <= 0) {
+      let msg = await message.channel.send("Invaild Arguments");
+      if (!Array.isArray(msg)) {
+        msg.delete(700);
+      }
+    } else {
+      let fetched = await message.channel.fetchMessages({ limit: amount });
+      message.channel
+        .bulkDelete(fetched)
+        .catch(error => message.channel.send(`Error: ${error}`));
+      let msg = await message.channel.send(
+        `Deleted \`${fetched.size}\` messages.`
+      );
+      if (!Array.isArray(msg)) {
+        msg.delete(700);
+      }
     }
-    
-    async run(bot: import("discord.js").Client, message: import("discord.js").Message, args: string[], prefix: string): Promise<void> {
-            message.delete();
-            if(isNaN(Number(args[0]))) {
-                message.channel.send("Invaild Arguments").then(msg => {
-                    if(!Array.isArray(msg)) {
-                        msg.delete(700);
-                    }
-                })
-               
-            } else {
-                let amount : number = Number(args[0]);
-                message.channel.fetchMessages({limit: amount}).then(fetched => {
-                    message.channel.bulkDelete(fetched);
-                    message.channel.send(`Deleted \`${fetched.size}\` messages.`).then(msg => {
-                        if(!Array.isArray(msg)) {
-                            msg.delete(700);
-                        }
-                    })
-                })
-            }      
-    }
-    
-}
+  }
+};
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,14 +1,21 @@
-import { Command } from "./base"
-import { Message, Client } from "discord.js"
+import { Command } from "./base";
+import { Message, Client } from "discord.js";
 
 module.exports = class ping extends Command {
-    constructor() {
-        super({name: "ping", description: "", usage: "", group: "member", aliases: []})
-    }
-    
-    async run(bot: Client, message: Message, args: string[], prefix: string): Promise<void> {
-        
-        message.channel.send(`Pong! \`${Math.abs(Date.now() - new Date(message.createdAt).getTime())}ms\``);
-    }
-    
-}
+  constructor() {
+    super({
+      name: "ping",
+      description: "",
+      usage: "",
+      group: "member",
+      aliases: []
+    });
+  }
+
+  async run(bot: Client, message: Message, args: string[], prefix: string): Promise<void> {
+    message.channel.send(
+      `Pong! \`${Math.abs(
+        Date.now() - new Date(message.createdAt).getTime()
+      )}ms\``);
+  }
+};
