29d25975b1fc1ebaf8e746386a4aeb5e814fce2d
SplitWolf <mat.jake1360@gmail.com>
2020-11-16 20:28:13 -0500

    fix: Fix UPR when serverQueue is null

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -18,12 +18,14 @@ module.exports = class skip extends Command {
     async run(bot: Client, message: Message, args: string[], prefix: string): Promise<void> {
         this.queues = new Map(Utils.queues);
         var serverQueue = this.queues.get(message.guild.id);
-
+        if(!serverQueue) {
+            message.channel.send(`I'm not playing anything.`);
+            return;
+        }
         if(serverQueue.dispatcher != null) {
-            
             serverQueue.dispatcher.end();
         } else {
-            message.channel.send(`I'm not playing anything.`)
+            message.channel.send(`I'm not playing anything.`);
         }
         
     }
